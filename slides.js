a0db81b21d7715a2f7ccd71b5cba6e01767ee402
qh-archives <syh394@nyu.edu>
2025-11-24 01:16:07 -0500

    Initial commit: Taiwan warp slider with 7 images

---
+++ slides.js
@@ -0,0 +1,65 @@
+export const slides = [
+  {
+    title: "Taipei, Taiwan",
+    description:
+      "The view outside my window looks so pretty during sunset.",
+    type: "Sunset",
+    field: "Home",
+    date: "2021",
+    image: "1.jpeg",
+  },
+  {
+    title: "Nantou, Taiwan",
+    description:
+      "Spotted a group of goats eating grass on the mountain.",
+    type: "Family Trip",
+    field: "Travel",
+    date: "2021",
+    image: "2.jpeg",
+  },
+  {
+    title: "Nantou, Taiwan",
+    description:
+      "The mountain feels so fresh up in the mountains.",
+    type: "Family Trip",
+    field: "Travel",
+    date: "2021",
+    image: "3.jpeg",
+  },
+  {
+    title: "Changhua, Taiwan",
+    description:
+      "Walking through the streets of where my grandparents live, and where I call home.",
+    type: "Grandparents",
+    field: "Home",
+    date: "2020",
+    image: "4.jpeg",
+  },
+  {
+    title: "Hualien, Taiwan",
+    description:
+      "The water is so blue and clear makes you think how beautiful Taiwan is.",
+    type: "Friend Trip",
+    field: "Travel",
+    date: "2021",
+    image: "5.jpeg",
+  },
+  {
+    title: "Taipei, Taiwan",
+    description:
+      "The lanterns light up Dihua Street, I remember buying fabric and looking up.",
+    type: "Wandering",
+    field: "Errands",
+    date: "2020",
+    image: "6.jpeg",
+  },
+  {
+    title: "Hsinchu, Taiwan",
+    description:
+      "A summer barbecue at my friends grandparents house, the country side is so peaceful and fun.",
+    type: "Friend Trip",
+    field: "Travel",
+    date: "2021",
+    image: "7.jpeg",
+  },
+];
